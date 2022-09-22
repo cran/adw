8d207b06687e8e9e44b07367adc54baef092c3d1
Panfeng Zhang <zhangpanfeng@cug.edu.cn>
2022-09-22 15:40:05 +0000

    version 0.2.0

---
+++ NEWS.md
@@ -0,0 +1,7 @@
+# adw 0.2.0
+
+-   all of the functions of sf package were replaced by the functions of terra package. The calculation speed will be several times faster than before.
+-   add parameters of extent, nmin, nmax, and maskON
+-   parameter gridSize were changed to gridsize
+-   delete parameters of xmin, xmax, ymin, ymax
+-   delete adw_land function. The new version of the function 'adw' adds a parameter 'maskON' to implement the function of the mask.
--- R/adw.R
+++ R/adw.R
@@ -1,81 +1,103 @@
-#' @title Angular Distance Weighting
+#' @title Angular Distance Weighting Interpolation.
 #' @description
-#' The irregularly-spaced data are interpolated onto regular latitude-longitude grids by weighting each station according to its distance and angle from the center of a search radius.
-#' @param dd a input dataframe which contains column names of lon, lat, value
-#' @param xmin the minimum longitude of the rectangular mesh
-#' @param xmax the maximum longitude of the rectangular mesh
-#' @param ymin the minimum latitude of the rectangular mesh
-#' @param ymax the maximum latitude of the rectangular mesh
-#' @param gridSize the grid resolution
-#' @param cdd the correlation decay distance, unit: meter
-#' @param m is used to adjust the weighting function further
-#' @return a regular latitude-longitude grid dataframe
+#' The irregularly-spaced data are interpolated onto regular latitude-longitude 
+#' grids by weighting each station according to its distance and angle from the 
+#' center of a search radius.
+#' @param ds a input dataframe which contains the column names of lon, lat, value
+#' @param extent a extent numeric vector of length 4 in the order c(xmin, xmax, ymin, ymax);
+#' or a SpatVector polygons object, assume that the coordinate reference system 
+#' is WGS1984 (EPSG: 4326); if extent is a NULL value (i.e. no extent is inputted), 
+#' the extent vector will be calculated from the input data.
+#' @param gridsize the grid size (resolution). units: degree.
+#' @param cdd correlation decay distance, i.e. the maximum search radius. 
+#' unit: meter. default value: 1e6.
+#' @param m is used to adjust the weighting function further, higher values of m
+#' increase the rate at which the weight decays with distance. default value 4.
+#' @param nmin the minimum number of observation points required to interpolate 
+#' a grid within the search radius (i.e. cdd); if the number of stations within 
+#' the search ridius (cdd) is less than nmin, a missing value will be generated  
+#' to fill this grid. default value 3.
+#' @param nmax The number of nearest points within the search radius to use for 
+#' interpolation. default value 10.
+#' @param maskON Logical value; whether to mask (remove) grids that are outside 
+#' the SpatVector polygon (extent). default TRUE. Parameter 'maskON' only works 
+#' when the class of parameter 'extent' is 'SpatVector'. 
+#' @return a regular latitude-longitude dataframe grid (interpoled values).
+#' @references Caesar, J., L. Alexander, and R. Vose, 2006: Large-scale changes in observed daily maximum and minimum temperatures: Creation and analysis of a new gridded data set. Journal of Geophysical Research, 111, https://doi.org/10.1029/2005JD006280.
 #' @examples
 #' set.seed(2)
 #' dd <- data.frame(lon = runif(100, min = 110, max = 117),
 #'                  lat = runif(100, min = 31, max = 37),
 #'                  value = runif(100, min = -10, max = 10))
 #' head(dd)
-#' dg <- adw(dd, gridSize = 1, cdd = 1e5)
-#' # dg is the dataframe of grid (mesh)
-#' head(dg)
-#' @importFrom sf st_as_sf st_buffer st_coordinates st_distance st_geometry
-#' @importFrom magrittr %>%
+#' 
+#' # example 1
+#' grd <- adw(dd, extent = c(110, 117, 31, 37), gridsize = 0.5, cdd = 1e5)
+#' head(grd)
+#' 
+#' # example 2
+#' urlmap <- "https://geo.datav.aliyun.com/areas_v3/bound/410000.json"
+#' hmap <- terra::vect(urlmap) # return a 'SpatVector' object.
+#' grd <- adw(dd, extent = hmap, gridsize = 0.5, cdd = 1e5)
+#' head(grd)
+#' 
+#' @importFrom terra ext vect mask buffer distance crds
 #' @importFrom geosphere bearing
-#' @importFrom stats na.omit
-#' @importFrom rnaturalearth ne_countries
+#' @importFrom methods is
 #' @export
+#' 
 
-adw <- function(dd, xmin = NULL, xmax = NULL, ymin = NULL, ymax = NULL,
-                gridSize = 1, cdd = 1000000, m = 4) {
-  requireNamespace("sf")
-  requireNamespace("geosphere")
-  requireNamespace("magrittr")
-  if(is.null(xmin)) {
-    xmin <- min(dd$lon)
-    xmax <- max(dd$lon)
-    ymin <- min(dd$lat)
-    ymax <- max(dd$lat)
+adw <- function(ds, extent = NULL, gridsize = 1, cdd = 1e6, 
+                      m = 4, nmin = 3, nmax = 10, maskON = TRUE) {
+  if (is(extent, "vector")) {
+    xmin = extent[1]
+    xmax = extent[2]
+    ymin = extent[3]
+    ymax = extent[4]
+  } else if (is(extent, "SpatVector")) {
+    bbox <- terra::ext(extent)
+    xmin = bbox[1]
+    xmax = bbox[2]
+    ymin = bbox[3]
+    ymax = bbox[4]
+  } else {
+    xmin <- min(ds$lon)
+    xmax <- max(ds$lon)
+    ymin <- min(ds$lat)
+    ymax <- max(ds$lat)
   }
-  # create grids
-  lon <- seq(xmin, xmax, gridSize)    # grid center
-  lat <- seq(ymin, ymax, gridSize)    # grid center
-  nlon <- length(lon)
-  nlat <- length(lat)
-  dg <- data.frame(lon = rep(lon, each = nlat),
-                   lat = rep(lat, times = nlon)) %>%
-    st_as_sf(coords = c("lon", "lat"), crs = 4326)
-  dg[, c("lon", "lat")] <- st_coordinates(dg)
+  dg <- expand.grid(lon = seq(xmin+gridsize/2, xmax, gridsize), 
+                    lat = seq(ymin+gridsize/2, ymax, gridsize)) |>
+    terra::vect(crs = "+proj=longlat +datum=WGS84", keepgeom = TRUE)
+  if (is(extent, "SpatVector") & maskON) {
+    dg <- terra::mask(dg, extent)
+  }
   dg[, "value"] <- NA
   ngrds <- nrow(dg)
-  ds <- na.omit(dd) %>% st_as_sf(coords = c("lon", "lat"), crs = 4326)
+  ds <- terra::vect(ds, geom = c("lon", "lat"), crs = "+proj=longlat +datum=WGS84")
   for (j in 1:ngrds) {
-    circle <- st_buffer(dg[j, ], dist = cdd) %>% st_geometry()
-    dx <- ds[circle,]
+    circle <- terra::buffer(dg[j,], width = cdd)
+    dx <- terra::mask(ds, circle)
     npts <- nrow(dx)  # station points numbers in the searh radius
-    if (npts > 10) {
-      dx[, "distance"] <- st_distance(dg[j,], dx) %>% as.numeric() # Units: [m]
-      dx <- dx[order(dx$distance), ]
-      dx <- dx[1:10, ]
+    if (npts > nmax) {
+      dx[, "distance"] <- terra::distance(dg[j,], dx)[1,]
+      dx <- dx[order(dx$distance),]
+      dx <- dx[1:nmax, ]
       r <- exp(1)^(-dx$distance/cdd)
       f <- r^m
-      theta <- bearing(c(dg$lon[j], dg$lat[j]), st_coordinates(dx))
-      theta <- theta * pi / 180
-      alpha <- rep(NA, 10)
-      
-      for (k in 1:10) {
+      theta <- geosphere::bearing(terra::crds(dg[j,]), terra::crds(dx)) * pi / 180
+      alpha <- rep(NA, nmax)
+      for (k in 1:nmax) {
         diffTheta <- theta[-k] - theta[k]
         alpha[k] <- sum(f[-k] * (1 - cos(diffTheta))) / sum(f[-k])
       }
       w <- f * (1 + alpha)
       dg[j, "value"] <- sum(dx$value * w) / sum(w)
-    }
-    else if (npts >= 3 & npts <= 10) {
-      dx[, "distance"] <- st_distance(dg[j,], dx) %>% as.numeric() # Units: [m]
+    } else if (npts >= nmin & npts <= nmax) {
+      dx[, "distance"] <- terra::distance(dg[j,], dx)[1,]
       r <- exp(1)^(-dx$distance/cdd)
       f <- r^m
-      theta <- bearing(c(dg$lon[j], dg$lat[j]), st_coordinates(dx))
-      theta <- theta * pi / 180
+      theta <- geosphere::bearing(terra::crds(dg[j,]), terra::crds(dx)) * pi / 180
       alpha <- rep(NA, npts)
       for (k in 1:npts) {
         diffTheta <- theta[-k] - theta[k]
@@ -83,10 +105,10 @@ adw <- function(dd, xmin = NULL, xmax = NULL, ymin = NULL, ymax = NULL,
       }
       w <- f * (1 + alpha)
       dg[j, "value"] <- sum(dx$value * w) / sum(w)
+    } else {
+      next
     }
   }
   dg <- as.data.frame(dg)
-  dg <- dg[, c("lon", "lat", "value")]
   return(dg)
 }
-
